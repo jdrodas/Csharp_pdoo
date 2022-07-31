e1cfa0afde0973af8032fbb84ccc68b195912d3f
Juan Dario Rodas M <jdrodas@hotmail.com>
2022-07-31 17:06:41 -0500

    Agregado proyecto con pruebas unitarias

---
+++ MonitoreoQuebradas/PruebasMonitoreo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasMonitoreo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasMonitoreo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da3f7c46-adc4-4db7-a524-0c82a756c466")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MonitoreoQuebradas/PruebasMonitoreo/PruebasQuebradas.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonitoreoQuebradas;
+
+namespace PruebasMonitoreo
+{
+    [TestClass]
+    public class PruebasQuebradas
+    {
+        /// <summary>
+        /// Evalua si hay 3 quebradas en estado crítico
+        /// </summary>
+        [TestMethod]        
+        public void EvaluaCantidadCritica()
+        {
+            //Arrange -- Preparar
+            Quebrada[] lasQuebradas = new Quebrada[5]
+            {
+                new Quebrada{Nombre = "La Picacha", Caudal =96 },
+                new Quebrada{Nombre = "La Presidenta", Caudal =20 },
+                new Quebrada{Nombre = "La Hueso", Caudal =92 },
+                new Quebrada{Nombre = "Santa Elena", Caudal =20 },
+                new Quebrada{Nombre = "La Iguaná", Caudal =81 }
+            };
+            //Act -- Actuar
+            int totalEsperado = 3;
+            int totalObtenido = Program.TotalQuebradasCriticas(lasQuebradas);
+
+            //Assert -- Valorar
+            Assert.AreEqual(totalEsperado, totalObtenido);
+        }
+
+        /// <summary>
+        /// Verifica que no se presenta condición de Emergencia por
+        /// la cantidad de quebradas en etado critico
+        /// </summary>
+        [TestMethod]
+        public void VerificaNoHayCondicionEmergencia()
+        {
+            //Arrange -- Preparar
+            Quebrada[] lasQuebradas = new Quebrada[5]
+            {
+                new Quebrada{Nombre = "La Picacha", Caudal =96 },
+                new Quebrada{Nombre = "La Presidenta", Caudal =20 },
+                new Quebrada{Nombre = "La Hueso", Caudal =92 },
+                new Quebrada{Nombre = "Santa Elena", Caudal =20 },
+                new Quebrada{Nombre = "La Iguaná", Caudal =80 }
+            };
+
+            //Act -- Actuar
+            bool condicionEsperada = false;
+            bool condicionObtenida = Program.EvaluaCondicionEmergencia(lasQuebradas);
+
+            //Assert -- Valorar
+            Assert.AreEqual(condicionEsperada, condicionObtenida);
+        }
+    }
+}
