f08424696a6d7e6e7d4a8740e4a8fe14971988e7
Juan Dario Rodas <jdrodas@hotmail.com>
2022-09-28 09:29:52 -0500

    Agregado el proyecto Poo_PS_Singleton

---
+++ Poo_PS_Singleton/Poo_PS_Singleton/AsignaEstacionador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_PS_Singleton
+{
+    public class AsignaEstacionador
+    {
+        //La instancia que se estará proporcionando cada vez que se invoque la clase
+        //Atributo de la clase
+        static AsignaEstacionador estacionador;
+        private string espacio;
+
+        //Constructor de la clase
+        protected AsignaEstacionador()
+        {
+            Random aleatorio = new Random(DateTime.Now.Millisecond);
+            string[] letras = { "Q", "Z", "H", "M", "A", "L", "G", "N", "X", "E"};
+
+            espacio = letras[aleatorio.Next(letras.Length)] + 
+                aleatorio.Next(10);
+        }
+
+        public static AsignaEstacionador ObtieneEspacioParqueo()
+        {
+            //Si no hay instancia creada, se hace una nueva instancia
+            if(estacionador == null)
+                estacionador = new AsignaEstacionador();
+
+            return estacionador;
+        }
+
+        public string Espacio
+        {
+            get { return espacio; }
+        }
+    }
+}
---
+++ Poo_PS_Singleton/Poo_PS_Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_PS_Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para demostrar el patrón GoF Singleton\n\n");
+
+            //Creamos dos instancias de la clase sin invocar el constructor
+            AsignaEstacionador espacio1 = AsignaEstacionador.ObtieneEspacioParqueo();
+            AsignaEstacionador espacio2 = AsignaEstacionador.ObtieneEspacioParqueo();
+
+            //Creamos dos instancias de la clase invocando directamente el constructor
+            //AsignaEstacionador espacio1 = new AsignaEstacionador();
+            //AsignaEstacionador espacio2 = new AsignaEstacionador();
+
+            if (espacio1 == espacio2)
+            {
+                Console.WriteLine("Los espacios de parqueo son los mismos. Unica instancia!");
+                Console.WriteLine($"El espacio asignado en espacio1 es {espacio1.Espacio}");
+                Console.WriteLine($"El espacio asignado en espacio2 es {espacio2.Espacio}");
+            }
+            else
+            {
+                Console.WriteLine("Son espacios de parqueo diferentes!");
+                Console.WriteLine($"El espacio asignado en espacio1 es {espacio1.Espacio}");
+                Console.WriteLine($"El espacio asignado en espacio2 es {espacio2.Espacio}");
+            }
+        }
+    }
+}
