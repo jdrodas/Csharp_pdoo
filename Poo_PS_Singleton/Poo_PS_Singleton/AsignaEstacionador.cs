112d41a7bc9651e1309dc7a60907621d814cd6c1
Juan Dario Rodas <jdrodas@hotmail.com>
2022-09-28 10:34:54 -0500

    Ajustes para evitar problemas del aleatorio generando el mismo valor

--- Poo_PS_Singleton/Poo_PS_Singleton/AsignaEstacionador.cs
+++ Poo_PS_Singleton/Poo_PS_Singleton/AsignaEstacionador.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Poo_PS_Singleton
 {
     public class AsignaEstacionador
     {
-        //La instancia que se estará proporcionando cada vez que se invoque la clase
         //Atributo de la clase
         static AsignaEstacionador estacionador;
+        static Random aleatorio;
         private string espacio;
 
-        //Constructor de la clase
-        protected AsignaEstacionador()
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        public AsignaEstacionador()
         {
-            Random aleatorio = new Random(DateTime.Now.Millisecond);
-            string[] letras = { "Q", "Z", "H", "M", "A", "L", "G", "N", "X", "E"};
+            aleatorio = new Random(DateTime.Now.Millisecond);
+            string[] letras = { "A", "B", "C", "D", "E", "F", "G", "H"};
 
             espacio = letras[aleatorio.Next(letras.Length)] + 
                 aleatorio.Next(10);
         }
 
+        /// <summary>
+        /// Obtiene la instancia utilizando el patrón Singleton
+        /// </summary>
+        /// <returns>instancia de la clase</returns>
         public static AsignaEstacionador ObtieneEspacioParqueo()
         {
             //Si no hay instancia creada, se hace una nueva instancia
@@ -32,6 +34,9 @@ namespace Poo_PS_Singleton
             return estacionador;
         }
 
+        /// <summary>
+        /// Obtiene el valor del espacio de parqueo
+        /// </summary>
         public string Espacio
         {
             get { return espacio; }
--- Poo_PS_Singleton/Poo_PS_Singleton/Program.cs
+++ Poo_PS_Singleton/Poo_PS_Singleton/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Poo_PS_Singleton
 {
@@ -10,16 +7,15 @@ namespace Poo_PS_Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programa para demostrar el patrón GoF Singleton\n\n");
+            Console.WriteLine("Programa para demostrar el patrón GoF Singleton");
 
             //Creamos dos instancias de la clase sin invocar el constructor
             AsignaEstacionador espacio1 = AsignaEstacionador.ObtieneEspacioParqueo();
+            Thread.Sleep(1000); //Esperamos un segundo...
             AsignaEstacionador espacio2 = AsignaEstacionador.ObtieneEspacioParqueo();
 
-            //Creamos dos instancias de la clase invocando directamente el constructor
-            //AsignaEstacionador espacio1 = new AsignaEstacionador();
-            //AsignaEstacionador espacio2 = new AsignaEstacionador();
-
+            //Comparamos las instancias para validar que son iguales
+            Console.WriteLine("\nComparamos las instancia espacio1 y espacio2...");
             if (espacio1 == espacio2)
             {
                 Console.WriteLine("Los espacios de parqueo son los mismos. Unica instancia!");
@@ -28,10 +24,32 @@ namespace Poo_PS_Singleton
             }
             else
             {
-                Console.WriteLine("Son espacios de parqueo diferentes!");
+                Console.WriteLine("Son espacios de parqueo diferentes. Diferentes instancias!");
                 Console.WriteLine($"El espacio asignado en espacio1 es {espacio1.Espacio}");
                 Console.WriteLine($"El espacio asignado en espacio2 es {espacio2.Espacio}");
             }
+
+            //Creamos dos instancias de la clase invocando directamente el constructor
+            AsignaEstacionador espacio3 = new AsignaEstacionador();
+            Thread.Sleep(1000); //Esperamos un segundo...
+            AsignaEstacionador espacio4 = new AsignaEstacionador();
+
+            //Comparamos las instancias para validar que NO son iguales
+            Console.WriteLine("\nComparamos las instancia espacio3 y espacio4...");
+            if (espacio3 == espacio4)
+            {
+                Console.WriteLine("Los espacios de parqueo son los mismos. Unica instancia!");
+                Console.WriteLine($"El espacio asignado en espacio3 es {espacio3.Espacio}");
+                Console.WriteLine($"El espacio asignado en espacio4 es {espacio4.Espacio}");
+            }
+            else
+            {
+                Console.WriteLine("Son espacios de parqueo diferentes!");
+                Console.WriteLine($"El espacio asignado en espacio3 es {espacio3.Espacio}");
+                Console.WriteLine($"El espacio asignado en espacio4 es {espacio4.Espacio}");
+            }
+
+            Console.WriteLine("\n\n\n");
         }
     }
 }
