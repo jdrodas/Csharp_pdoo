dd93cf21b1ac8921640aded4fa1a7f02d3b538da
Juan Dario Rodas <jdrodas@hotmail.com>
2022-10-20 06:15:33 -0500

    Agregado Poo_PS_Adaptador

---
+++ Poo_PS_Adaptador/Poo_PS_Adaptador/Compuesto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_PS_Adaptador
+{
+    // Esta es la clase "Target"
+    public class Compuesto
+    {
+        protected float puntoEbullicion;
+        protected float puntoCongelacion;
+        protected double pesoMolecular;
+        protected string formulaQuimica;
+        
+
+        public virtual string InformacionCompuesto()
+        {
+            string informacion = "Este compuesto no tiene nada";
+            return informacion;
+        }
+
+    }
+}
---
+++ Poo_PS_Adaptador/Poo_PS_Adaptador/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_PS_Adaptador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para demostrar el patrón GoF Adaptador");
+
+            //Console.WriteLine("Información para el agua:");
+            ////Aqui se hace lo que Ana María responda
+            //Compuesto elAgua = new CompuestoEnriquecido("agua");
+            //Console.WriteLine($"\n{elAgua.InformacionCompuesto()}");
+
+            //Compuesto elVodka = new CompuestoEnriquecido("vodka");
+            //Console.WriteLine($"\n{elVodka.InformacionCompuesto()}");
+
+            string[] nombreCompuestos = { "agua", "benzeno", "etanol", "cloruro de magnesio" };
+
+            Compuesto[] losCompuestos = new CompuestoEnriquecido[nombreCompuestos.Length];
+
+            for (int i = 0; i < losCompuestos.Length; i++)
+            {
+                losCompuestos[i] = new CompuestoEnriquecido(nombreCompuestos[i]);
+                Console.WriteLine($"\n{losCompuestos[i].InformacionCompuesto()}");
+            }
+        }
+    }
+}
