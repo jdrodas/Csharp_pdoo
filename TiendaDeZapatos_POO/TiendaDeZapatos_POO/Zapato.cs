07f6efae63f0f3b2e1faa5aa7c1c0bfdfecf2277
Juan Dario Rodas <jdrodas@hotmail.com>
2023-01-23 17:38:24 -0500

    Agregados métodos adicionales en la clase Zapato para acceder a los valores de los atributos

--- TiendaDeZapatos_POO/TiendaDeZapatos_POO/Zapato.cs
+++ TiendaDeZapatos_POO/TiendaDeZapatos_POO/Zapato.cs
@@ -21,7 +21,7 @@ namespace TiendaDeZapatos_POO
         /// Asigna el valor del atributo Talla
         /// </summary>
         /// <param name="datoTalla">El dato de la talla</param>
-        public void AsignaTalla(int datoTalla)
+        public void SetTalla(int datoTalla)
         {
             if (datoTalla >= 28 && datoTalla <= 44)
                 talla = datoTalla;
@@ -33,24 +33,70 @@ namespace TiendaDeZapatos_POO
         /// Obtiene el valor del atributo Talla
         /// </summary>
         /// <returns>La talla asignada</returns>
-        public int ObtieneTalla()
+        public int GetTalla()
         {
             return talla;
         }
 
+        /// <summary>
+        /// Obtiene el valor del atributo Estilo
+        /// </summary>
+        /// <returns>El Estilo asignado</returns>
+        public string GetEstilo()
+        {
+            return estilo;
+        }
+
+        /// <summary>
+        /// Asigna el valor del atributo Talla
+        /// </summary>
+        /// <param name="datoEstilo">El dato del estilo</param>
+        public void SetEstilo(string datoEstilo)
+        {
+            estilo = datoEstilo;
+        }
+
+        /// <summary>
+        /// Obtiene el valor del atributo Color
+        /// </summary>
+        /// <returns>El Estilo asignado</returns>
+        public string GetColor()
+        {
+            return color;
+        }
+
+        /// <summary>
+        /// Asigna el valor del atributo Color
+        /// </summary>
+        /// <param name="datoColor">El dato del color</param>
+        public void SetColor(string datoColor)
+        {
+            color = datoColor;
+        }
+
         //Zona de Propiedades
+        
+        /// <summary>
+        /// Obtiene o asigna el valor del estilo
+        /// </summary>
         public string Estilo
         {
             get { return estilo; }
             set { estilo = value; }
         }
 
+        /// <summary>
+        /// Obtiene o asigna el valor del color
+        /// </summary>
         public string Color
         {
             get { return color; }
             set { color = value; }
         }
 
+        /// <summary>
+        /// Obtiene o asigna el valor de la talla
+        /// </summary>
         public int Talla
         {
             get { return talla; }
